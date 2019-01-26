e06896902b1c6d3c62b83b613ff80ef3b0a95517
WeroLopez <werorifaycontrola@gmail.com>
2019-01-26 08:47:51 -0800

    Instanciador

---
+++ Assets/Scripts/CoreGame/Instanciador.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoreGame
+{
+    namespace Instanciador
+    {
+        public class Instanciador : MonoBehaviour
+        {
+            [SerializeField]
+            GameObject @object;
+            SpriteRenderer sr;
+
+            // Start is called before the first frame update
+            void Start()
+            {   
+                sr = GetComponent<SpriteRenderer>();
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+
+            }
+
+            private void OnTriggerEnter2D(Collider2D other)
+            {
+                if (other.name.StartsWith("Instanciador"))
+                {
+                    @object.SetActive(true);
+                }
+            }
+        }
+    }
+}
