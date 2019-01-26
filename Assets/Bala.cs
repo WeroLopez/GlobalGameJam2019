9081a5d698a0ebdafe48257242c894bc5046328d
Armando Meza Leyva <armezaleyva@gmail.com>
2019-01-26 11:32:00 -0700

    Comentarios y pruebas con la bala

---
+++ Assets/Bala.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#pragma warning disable 0649
+public class Bala : MonoBehaviour
+{
+    float shotSpeed = 15f;
+    Rigidbody2D bulletRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRigidBody = GetComponent<Rigidbody2D>();
+
+        bulletRigidBody.velocity = new Vector2(0, shotSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // La bala choca con el jugador
+        if (other.name.StartsWith("Player"))
+        {
+            print("DISPARO");
+        }
+    }
+}
--- Assets/Scripts/Enemies/Carro.cs
+++ Assets/Scripts/Enemies/Carro.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#pragma warning disable 0649
 public class Carro : MonoBehaviour
 {
     // Carro
@@ -11,27 +12,34 @@ public class Carro : MonoBehaviour
     float moveSpeed;
     [SerializeField]
     bool derechaAIzquierda;
+    [SerializeField]
+    bool puedeDisparar;
 
     // Start is called before the first frame update
     void Start()
     {
         carRigidBody = GetComponent<Rigidbody2D>();
+        
+        // El carro se mueve a la izquierda
+        if (!derechaAIzquierda) {
+            carRigidBody.velocity = new Vector2(-moveSpeed, 0);
+        } 
+        // El carro se mueve a la derecha
+        else {
+            carRigidBody.velocity = new Vector2(moveSpeed, 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!derechaAIzquierda) {
-            carRigidBody.velocity = new Vector2(-moveSpeed, 0);
-        } 
-        else {
-            carRigidBody.velocity = new Vector2(moveSpeed, 0);
-        }
+        
         
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // El carro choca con el jugador
         if (other.name.StartsWith("Player"))
         {
             print("COLISION");
